0dbd5b9bcad0d5b8c67e98d9ce79b2d8553a05cf
Vladimir Migashko <migashko@mamba.ru>
2012-12-20 21:37:18 +0400

    btp2 debug

--- vset/vtree/aspect/ad_insert_value.hpp
+++ vset/vtree/aspect/ad_insert_value.hpp
@@ -26,13 +26,16 @@ struct ad_insert_value
     container_type& container = t.get_container();
     // находим ближайший подходящий нод
     container_iterator itr = t.get_aspect().template get<_lower_node_>()(t, value);
+    volatile int debug = 0;
     if ( itr == container.end() )
     {
+      volatile int debug = 1;
       // Сюда попадем если ниодного нода еще не созданно
       itr = t.get_aspect().template get<_create_node_>()(t, value);
     }
     else
     {
+      volatile int debug = 2;
       // Ищем ближайший свободный но
       itr = t.get_aspect().template get<_first_proper_node_>()(t, itr);
     }
