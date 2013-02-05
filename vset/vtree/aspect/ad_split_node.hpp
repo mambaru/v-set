309b39f0a5d0c5f09a87dde8899b79b8e54df50e
migashko <migashko@gmail.com>
2013-02-06 02:08:46 +0400

    btp2 vset deb

--- vset/vtree/aspect/ad_split_node.hpp
+++ vset/vtree/aspect/ad_split_node.hpp
@@ -20,7 +20,7 @@ struct ad_split_node
     typedef typename T::container_type container_type;
     typedef typename container_type::iterator iterator;
   };
-  
+
   template<typename T>
   typename helper<T>::iterator
   operator()(T& t, typename helper<T>::iterator itr, const typename T::value_type& value)
@@ -32,22 +32,22 @@ struct ad_split_node
     typedef typename T::allocator_type allocator_type;
     typedef typename allocator_type::value_type array_type;
     typedef typename allocator_type::pointer    array_pointer;
-    
+
     container_type& container = t.get_container();
 
     if ( itr == container.end() )
       return itr;
-    
+
     array_pointer arr1 = itr->second;
     array_pointer arr2 = t.get_allocator().allocate(1);
     if ( !arr2) // == 0 ambigous
-      return container.end(); // TODO: проверить что offset_pointer при сравнении с числом не сравнивает оффыеты
-      
+      abort();//return container.end(); // TODO: проверить что offset_pointer при сравнении с числом не сравнивает оффыеты
+
     size_t offset = arr1->size();
-    
+
     if ( offset < 2 )
       return itr;
-    
+
     offset /= 2;
 
     arr2->assign(
@@ -65,7 +65,7 @@ struct ad_split_node
     container.erase( itr );
     container_iterator itr1 = t.get_aspect().template get<_insert_to_container_>()(t, std::make_pair(arr1->front(), arr1->back()), arr1);
     container_iterator itr2 = t.get_aspect().template get<_insert_to_container_>()(t, std::make_pair(arr2->front(), arr2->back()), arr2);
-    
+
     itr = t.get_aspect().template get<_node_for_insert_>()(t, itr1, itr2, value);
 
     arr1 = itr->second;
