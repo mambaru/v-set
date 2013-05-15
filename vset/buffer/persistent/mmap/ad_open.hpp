6e6372e25e22514006a9c9b0ce4acd87e03f4d05
Dmitry Saprykin <saprykin.dmitry@gmail.com>
2013-05-15 19:49:36 +0400

    * madvice

--- vset/buffer/persistent/mmap/ad_open.hpp
+++ vset/buffer/persistent/mmap/ad_open.hpp
@@ -62,9 +62,7 @@ struct ad_open
     if ( is_created )
       ::memset(data, 0, file_size);
 
-    int ret;
-    ::madvise(data, 0, MADV_RANDOM);
-    if( ret < 0 )
+    if( ::madvise(data, 0, MADV_RANDOM) < 0 )
       throw;
 
     t.get_aspect().template get<_buffer_size_>() = file_size;
