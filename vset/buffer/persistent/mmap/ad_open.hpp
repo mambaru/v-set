172a5eb59f9c9b00cb0ba0b8fdec004327121e78
Dmitry Saprykin <saprykin.dmitry@gmail.com>
2013-05-16 12:01:37 +0400

    * reverted vset advice

--- vset/buffer/persistent/mmap/ad_open.hpp
+++ vset/buffer/persistent/mmap/ad_open.hpp
@@ -62,9 +62,6 @@ struct ad_open
     if ( is_created )
       ::memset(data, 0, file_size);
 
-    if( ::madvise(data, 0, MADV_RANDOM) < 0 )
-      throw;
-
     t.get_aspect().template get<_buffer_size_>() = file_size;
     t.get_aspect().template get<_buffer_>() = data;
     head_type* head = t.get_aspect().template get<_head_>()(t);
