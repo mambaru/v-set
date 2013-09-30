2e653a88c2a33233b5efb5a4eb7b1193bcce1347
Vladimir Migashko <migashko@mamba.ru>
2013-09-30 18:17:38 +0400

    vset bench compare upd

--- benchmark/compare.cpp
+++ benchmark/compare.cpp
@@ -5,6 +5,7 @@
 #include <fas/xtime.hpp>
 #include <fas/typemanip.hpp>
 #include <iostream>
+#include <set>
 
 #include "config.hpp"
 //using namespace vset;
@@ -14,6 +15,9 @@ struct data
   int data1;
   int data2;
   int data3;
+  int data4;
+  int data5;
+  int data6;
 };
 
 
@@ -26,7 +30,10 @@ bool operator == (data l, data r)
 typedef fas::type_list_n<
   vset::compare< fas::member<data, int, &data::data1>, std::less<int> >,
   vset::compare< fas::member<data, int, &data::data2>, std::greater<int> >,
-  vset::compare< fas::member<data, int, &data::data3>, std::less<int> >
+  vset::compare< fas::member<data, int, &data::data3>, std::less<int> >,
+  vset::compare< fas::member<data, int, &data::data4>, std::less<int> >,
+  vset::compare< fas::member<data, int, &data::data5>, std::less<int> >,
+  vset::compare< fas::member<data, int, &data::data6>, std::less<int> >
 >::type compare_list;
 
 struct cmp_data1: vset::compare_list<compare_list> {};
@@ -40,40 +47,68 @@ struct cmp_data3
     //std::cout << l.data1 << std::endl;
     return l.data1 < r.data1 ||
     ( ! ( r.data1 < l.data1 ) && l.data2 > r.data2 ) ||
-    ( ! ( r.data1 < l.data1 ) && ! ( r.data2 > l.data2 ) && l.data3 < r.data3 );
+    ( ! ( r.data1 < l.data1 ) && ! ( r.data2 > l.data2 ) && l.data3 < r.data3 ) ||
+    ( ! ( r.data1 < l.data1 ) && !( r.data2 > l.data2 ) && !( r.data3 < l.data3 ) && l.data4 < r.data4 ) ||
+    ( ! ( r.data1 < l.data1 ) && !( r.data2 > l.data2 ) && !( r.data3 < l.data3 ) && !( r.data4 < l.data4 ) && l.data5 < r.data5 ) ||
+    ( ! ( r.data1 < l.data1 ) && !( r.data2 > l.data2 ) && !( r.data3 < l.data3 ) && !( r.data4 < l.data4 ) && !( r.data5 < l.data5 ) && l.data6 < r.data6 )
+    ;
   }
 };
 
 
+#define MAX_TEST 10
 #define MAX_DATA 10000000
 int main()
 {
-  std::vector<data> d1, d2, d3;
-  d1.resize(MAX_DATA);
-  std::generate(d1.begin(), d1.end(), [](){return data{std::rand()%100, std::rand()%100, std::rand()%100};});
-  d2 = d1;
-  d3 = d1;
+  std::srand(time(0));
+  std::vector<data> d;
+  d.resize(MAX_DATA);
+  std::generate(d.begin(), d.end(), [](){return data{std::rand()%100, std::rand()%100, std::rand()%100, std::rand()%100, std::rand()%100, std::rand()%100};});
   
-  fas::nanospan start = fas::nanotime();
-  std::sort(d1.begin(), d1.end(), cmp_data1());
-  fas::nanospan finish = fas::nanotime();
-  std::cout << finish - start << std::endl;
-  
-  start = fas::nanotime();
-  std::sort(d2.begin(), d2.end(), cmp_data2());
-  finish = fas::nanotime();
-  std::cout << finish - start << std::endl;
+  std::set<fas::nanospan> time1;
+  std::set<fas::nanospan> time2;
+  std::set<fas::nanospan> time3;
+  for (int i = 0; i < MAX_TEST; ++i)
+  {
+    std::cout << "---------------------------" << std::endl;
+    std::vector<data> d1, d2, d3;
+    
+    d1 = d;
+    d2 = d;
+    d3 = d;
 
-  start = fas::nanotime();
-  std::sort(d3.begin(), d3.end(), cmp_data3());
-  finish = fas::nanotime();
-  std::cout << finish - start << std::endl;
+    fas::nanospan start = fas::nanotime();
+    std::sort(d1.begin(), d1.end(), cmp_data1());
+    fas::nanospan finish = fas::nanotime();
+    std::cout << finish - start << std::endl;
+    time1.insert(finish - start);
 
-  if ( !(d1==d2) )
-    abort();
+    start = fas::nanotime();
+    std::sort(d2.begin(), d2.end(), cmp_data2());
+    finish = fas::nanotime();
+    std::cout << finish - start << std::endl;
+    time2.insert(finish - start);
 
-  if ( !(d1==d3) )
-    abort();
+    start = fas::nanotime();
+    std::sort(d3.begin(), d3.end(), cmp_data3());
+    finish = fas::nanotime();
+    std::cout << finish - start << std::endl;
+    time3.insert(finish - start);
+
+    if ( !(d1==d2) )
+      abort();
+
+    if ( !(d1==d3) )
+      abort();
+  }
+
+  std::cout << "---------------------------" << std::endl;
+  std::cout << "---------------------------" << std::endl;
+  std::cout << "---------------------------" << std::endl;
+
+  std::cout << *(time1.begin()) << std::endl;
+  std::cout << *(time2.begin()) << std::endl;
+  std::cout << *(time3.begin()) << std::endl;
 
   return 0;
 }
