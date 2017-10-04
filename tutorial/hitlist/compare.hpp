46e6d0145180b263a6333bfd4fe0ca47f788f244
Vladimir Migashko <migashko@gmail.com>
2017-10-04 03:34:17 +0300

    code

--- tutorial/hitlist/compare.hpp
+++ tutorial/hitlist/compare.hpp
@@ -13,13 +13,13 @@ struct cmp_by_src:
 struct cmp_by_dst:
   vset::compare_list<
     vset::compare_member< hit, uint32_t, &hit::dst_id, std::less<uint32_t> >,
-    vset::compare_member< hit, time_t, &hit::ts, std::greater<time_t> >,
+    vset::compare_member< hit, time_t,   &hit::ts,     std::greater<time_t> >,
     vset::compare_member< hit, uint32_t, &hit::src_id, std::less<uint32_t> >
   > {};
 
 struct cmp_by_ts:
   vset::compare_list<
-    vset::compare_member< hit, time_t, &hit::ts, std::greater<time_t> >,
+    vset::compare_member< hit, time_t,   &hit::ts,     std::greater<time_t> >,
     vset::compare_member< hit, uint32_t, &hit::src_id, std::less<uint32_t> >,
     vset::compare_member< hit, uint32_t, &hit::dst_id, std::less<uint32_t> >
   > {};
