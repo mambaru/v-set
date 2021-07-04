4e918d3721bbbb31b003adfadb0ba54a3d4ca82a
GitLab Runner <gitlab-runner@repobuild3.lan>
2021-07-04 20:37:17 +0300

    Generated export autocommit from mamba.ru

--- benchmark/config.hpp
+++ benchmark/config.hpp
@@ -2,14 +2,13 @@
 #define VSET_BENCHMARK_CONFIG_HPP
 
 #ifdef NDEBUG
-const int MAX_COUNT = 10240000;
-const int MIN_COUNT = 10000;
-const int CHUNK_SIZE = 512;
+#define MAX_COUNT 10240000
+#define MIN_COUNT 10000
+#define CHUNK_SIZE 512
 #else
-const int MAX_COUNT = 1024;
-const int MIN_COUNT = 16;
-const int CHUNK_SIZE = 512;
+#define MAX_COUNT 1024
+#define MIN_COUNT 16
+#define  CHUNK_SIZE 512
 #endif
-
 
 #endif
